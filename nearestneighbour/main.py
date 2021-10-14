c6ba17ec5a184e60f7e94abd8d05e7bbe702468d
Jamie Mair <jamie.mair@hotmail.co.uk>
2021-10-14 13:08:17 +0100

    Added more methods to implement, along with some tests

--- nearestneighbour/main.py
+++ nearestneighbour/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def generate_point_cloud(n:int, d:int = 2, seed=1234) -> np.ndarray:
@@ -20,7 +21,7 @@ def generate_point_cloud(n:int, d:int = 2, seed=1234) -> np.ndarray:
 
 
 def find_nearest_neighbour_from_point(point_cloud:np.ndarray, point:int) -> int:
-    """Finds the index of the point which is nearest to a given point, specified by an index
+    """Finds the index of the point in a given point cloud which is nearest to an input point, specified by an index
 
     Args:
         point_cloud (np.ndarray): The point cloud data stored in a n x d array, where n is the number of points and d is the dimensionality of each point.
@@ -30,3 +31,82 @@ def find_nearest_neighbour_from_point(point_cloud:np.ndarray, point:int) -> int:
         int: The index of the nearest point
     """
     pass
+
+
+def find_all_nearest_neighbours(point_cloud:np.ndarray) -> np.ndarray:
+    """Finds the nearest neighbour of each point in the point cloud and returns the index of that point in a list.
+
+
+    Args:
+        point_cloud (np.ndarray): The point cloud data stored in a n x d array, where n is the number of points and d is the dimensionality of each point.
+
+    Returns:
+        np.ndarray: An n dimensional array array of integers. Each element corresponds to a point in the point cloud, and the value contained is the index
+        of the nearest point in the point_cloud array.
+    """
+    pass
+
+
+def plot_point_cloud(point_cloud:np.ndarray):
+    """Plots a graph showing the point cloud. The point cloud must have a dimensionality less than or equal to 3.
+
+    Args:
+        point_cloud (np.ndarray): The point cloud data stored in a n x d array, where n is the number of points and d is the dimensionality of each point. d must be less than 4.
+    """
+
+    n, d = np.shape(point_cloud)
+    assert d <= 3, "In order to plot, the points must be less than 4D."
+
+    fig = plt.figure()
+    if d == 1:
+        __plot_point_cloud_1D(point_cloud)
+    elif d==2:
+        __plot_point_cloud_2D(point_cloud)
+    elif d==3:
+        __plot_point_cloud_3D(point_cloud)
+    else:
+        raise Exception("Unrecognised dimensionality for plotting.")
+    return fig
+
+
+def __plot_point_cloud_1D(point_cloud:np.ndarray):
+    """Plots a graph showing a 1 dimensional point cloud.
+
+    Args:
+        point_cloud (np.ndarray): The point cloud data stored in a n x 1 array, where n is the number of points.
+    """
+    pass
+
+
+def __plot_point_cloud_2D(point_cloud:np.ndarray):
+    """Plots a graph showing a 2 dimensional point cloud.
+
+    Args:
+        point_cloud (np.ndarray): The point cloud data stored in a n x 2 array, where n is the number of points.
+    """
+    pass
+
+
+def __plot_point_cloud_3D(point_cloud:np.ndarray):
+    """Plots a graph showing a 3 dimensional point cloud.
+
+    Args:
+        point_cloud (np.ndarray): The point cloud data stored in a n x 3 array, where n is the number of points.
+    """
+    pass
+
+def plot_2D_nearest_neighbours(point_cloud:np.array, nearest_neighbours:np.ndarray=None):
+    """Plots a 2D scatter graph of a point cloud, withs arrows from each point to their nearest neighbour.
+
+    Args:
+        point_cloud (np.array): The point cloud data stored in a n x 2 array, where n is the number of points.
+        nearest_neighbours (np.array, optional): An n dimensional array array of integers. Each element corresponds to a point in the point cloud, and the value contained is the index
+        of the nearest point in the point_cloud array. Defaults to None.
+    """
+    n, d = np.shape(point_cloud)
+    assert d==2, "Can only plot nearest neighbours for a 2D point cloud."
+    # If the nearest neighbours are not already found, re-calculate.
+    if nearest_neighbours is None:
+        nearest_neighbours = find_all_nearest_neighbours(point_cloud)
+    
+    # TODO: Implement plotting here
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 from nearestneighbour import *
 import numpy as np
+from time import time
 
 
 def test_point_cloud_generation():
@@ -27,4 +28,35 @@ def test_nearest_neighbour():
     test_answers = [24, 112, 140, 4, 231, 143]
     
     for (point, answer) in zip(test_points, test_answers):
-        assert find_nearest_neighbour_from_point(point_cloud, point)==answer
+        calculated_nn = find_nearest_neighbour_from_point(point_cloud, point)
+        assert calculated_nn==answer, f"The nearest neighbour of point {point} was expected to be {answer}, but was calculated to be {calculated_nn}."
+
+
+def test_all_nearest_neighbours():
+    n = 256
+    point_cloud = generate_point_cloud(256, d=3, seed=1234)
+
+    nearest_neighbour_indices = find_all_nearest_neighbours(point_cloud)
+    for i in range(n):
+        nearest_neighbour_idx = find_nearest_neighbour_from_point(point_cloud, i)
+        assert nearest_neighbour_idx == nearest_neighbour_indices[i], f"Failed on the index {i}. Actual: {nearest_neighbour_idx}, Calculated: {nearest_neighbour_indices[i]}"
+
+
+def test_all_nearest_neighbours_speed():
+    n = 8096
+    repeats = 3
+    point_cloud = generate_point_cloud(n, d=3, seed=1234)
+
+    times = []
+    for r in range(repeats):
+        start_t = time()
+        nearest_neighbour_indices = find_all_nearest_neighbours(point_cloud)
+        end_t = time()
+        if nearest_neighbour_indices is None:
+            raise Exception("The find_all_nearest_neighbours function is not implemented properly, returned a None object.")
+        times.append(end_t-start_t)
+
+    min_time = np.min(times)
+    print(f"Minimum NN calculation time for n={n} is {min_time*1000:.3f}ms.\n")
+    threshold = 2.5
+    assert min_time < threshold, f"NN calculation for n={n} must be below {threshold*1000:.1f}ms"
